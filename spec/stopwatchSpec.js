ad0d988b2df00383ba07d11ebc400496098face4
Nate <namoricoo@hotmail.com>
2014-02-08 21:09:31 -0800

    write spec to see if stopwatch isRunning variable is defined

--- spec/stopwatchSpec.js
+++ spec/stopwatchSpec.js
@@ -8,5 +8,9 @@ describe("Stopwatch", function() {
 		it('should have prototype Stopwatch defined', function () {
 			expect(Stopwatch).toBeDefined();
 		});
+
+		it('should know if the timer is running', function() {
+			expect(Stopwatch.isRunning).toBeDefined();
+		});
 	});
 });
